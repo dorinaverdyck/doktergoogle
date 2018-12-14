fa48b19c44613291dd3795ec44a9c710ac61cbdd
Sam De Maeyer <sam@showmyhomework.co.uk>
2018-12-14 11:04:19 +0000

    i18n first draught

--- main.js
+++ main.js
@@ -1,98 +1,112 @@
-const welcomeScreen = document.querySelector('#welcome');
-const symptomScreen = document.querySelector('#symptoms');
-const diagnoseScreen = document.querySelector('#diagnose');
-const prescriptionScreen = document.querySelector('#prescription');
+const welcomeScreen = document.querySelector("#welcome");
+const symptomScreen = document.querySelector("#symptoms");
+const diagnoseScreen = document.querySelector("#diagnose");
+const prescriptionScreen = document.querySelector("#prescription");
+let locale = 'nl';
 
 function choice(l) {
-return l[Math.floor(Math.random() * l.length)];
+  return l[Math.floor(Math.random() * l.length)];
 }
 
 function hideElements(elements) {
-elements.forEach(element => element.style.display = 'none');
+  elements.forEach(element => (element.style.display = "none"));
 }
 
 function showElements(elements) {
-elements.forEach(element => element.style.display = 'flex');
+  elements.forEach(element => (element.style.display = "flex"));
 }
 
 function goToSymptomScreen() {
-hideElements([welcomeScreen, diagnoseScreen, prescriptionScreen]);
-showElements([symptomScreen]);
+  hideElements([welcomeScreen, diagnoseScreen, prescriptionScreen]);
+  showElements([symptomScreen]);
 }
 
 function goToDiagnoseScreen() {
-hideElements([welcomeScreen, symptomScreen, prescriptionScreen]);
-showElements([diagnoseScreen]);
-onGenerate();
+  hideElements([welcomeScreen, symptomScreen, prescriptionScreen]);
+  showElements([diagnoseScreen]);
+  onGenerate();
 }
 
 function goToWelcomeScreen() {
-hideElements([symptomScreen, diagnoseScreen, prescriptionScreen]);
-showElements([welcomeScreen]);
+  hideElements([symptomScreen, diagnoseScreen, prescriptionScreen]);
+  showElements([welcomeScreen]);
 }
 
 function goToPrescriptionScreen() {
-hideElements([welcomeScreen, symptomScreen, diagnoseScreen]);
-showElements([prescriptionScreen]);
-const imageUrl = choice(PRESCRIPTIONS);
-document.querySelector('.prescription-image').src = imageUrl;
+  hideElements([welcomeScreen, symptomScreen, diagnoseScreen]);
+  showElements([prescriptionScreen]);
+  const imageUrl = choice(PRESCRIPTIONS);
+  document.querySelector(".prescription-image").src = imageUrl;
 }
+
+
 
 const PRESCRIPTIONS = [
-'images/voorschrift-1.svg',
-'images/voorschrift-1.svg',
-'images/voorschrift-4.svg',
-]
+  "images/voorschrift-1.svg",
+  "images/voorschrift-2.svg",
+  "images/voorschrift-3.svg"
+];
 
 function printPrescription() {
-window.print();
-goToWelcomeScreen();
+  window.print();
+  goToWelcomeScreen();
 }
 
-const SKETCH_ID = '-LTWTm__dntsH9NS-5ZE';
+function switchLanguage(e, newLocale) {
+  e.preventDefault();
+  document.querySelectorAll('.i18n').forEach(img => (img.src = img.src.replace(locale, newLocale)));
+  locale = newLocale;
+}
+
+const SKETCH_ID = "-LTWTm__dntsH9NS-5ZE";
 
 async function fetchSketch(sketchId) {
-const url = `https://emrg-pcg.firebaseio.com/sketch/${SKETCH_ID}.json`;
-const res = await fetch(url);
-return await res.json();
+  const url = `https://emrg-pcg.firebaseio.com/sketch/${SKETCH_ID}.json`;
+  const res = await fetch(url);
+  return await res.json();
 }
 
 async function loadSketch(seed) {
-const sketch = await fetchSketch(SKETCH_ID);
-const phraseBook = await parsePhraseBook(sketch.source, fetchSketch);
-return phraseBook;
+  const sketch = await fetchSketch(SKETCH_ID);
+  const phraseBook = await parsePhraseBook(sketch.source, fetchSketch);
+  return phraseBook;
 }
 
 async function onGenerate() {
-if (!window.sketch) return;
-const result = await generateString(window.sketch, 'root', {}, Date.now());
-const ziekte = /U heeft last van (een\s+)?(.*?)\./.exec(result)[2];
+  if (!window.sketch) return;
+  const result = await generateString(window.sketch, "root", {}, Date.now());
+  const ziekte = /U heeft last van (een\s+)?(.*?)\./.exec(result)[2];
 
-// console.log(result);
+  // console.log(result);
 
-document.querySelector('.diagnose-seed').innerHTML = result;
-document.querySelector('.diagnose-ziekte').innerHTML = ziekte;
+  document.querySelector(".diagnose-seed").innerHTML = result;
+  document.querySelector(".diagnose-ziekte").innerHTML = ziekte;
 }
 
 async function init() {
-window.sketch = await loadSketch(SKETCH_ID);
-// document.querySelector('#generate').addEventListener('click', onGenerate);
-onGenerate();
+  window.sketch = await loadSketch(SKETCH_ID);
+  // document.querySelector('#generate').addEventListener('click', onGenerate);
+  onGenerate();
 }
 
 init();
 
-const $title = document.querySelector('.title');
-$title.addEventListener('click', goToSymptomScreen);
+const $title = document.querySelector(".main-logo");
+$title.addEventListener("click", goToSymptomScreen);
 
-const $diagnoseButton = document.querySelector('#diagnose-button');
-$diagnoseButton.addEventListener('click', goToDiagnoseScreen);
+const $diagnoseButton = document.querySelector("#diagnose-button");
+$diagnoseButton.addEventListener("click", goToDiagnoseScreen);
 
-const $prescriptionButton = document.querySelector('#prescription-button');
-$prescriptionButton.addEventListener('click', goToPrescriptionScreen);
+const $prescriptionButton = document.querySelector("#prescription-button");
+$prescriptionButton.addEventListener("click", goToPrescriptionScreen);
 
-const $printButton = document.querySelector('#print-button');
-$printButton.addEventListener('click', printPrescription);
+const $printButton = document.querySelector("#print-button");
+$printButton.addEventListener("click", printPrescription);
 
-const $resetButton = document.querySelector('#reset-button');
-$resetButton.addEventListener('click', goToWelcomeScreen);
+const $resetButton = document.querySelector("#reset-button");
+$resetButton.addEventListener("click", goToWelcomeScreen);
+
+// languageNl.addEventListener("click", switchLanguage('nl'));
+// languageFr.addEventListener("click", switchLanguage('fr'));
+// languageEn.addEventListener("click", switchLanguage('en'));
+// languageDe.addEventListener("click", switchLanguage('de'));
